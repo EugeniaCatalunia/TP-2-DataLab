9baef4e9a4a614ac225db2a915e90043484e618c
EugeniaCatalunia <136481279+EugeniaCatalunia@users.noreply.github.com>
2023-06-13 21:54:26 -0300

    Add files via upload

---
+++ Java/codigo.js
@@ -0,0 +1,12 @@
+document.getElementById('menu').addEventListener('click', function () {
+
+    document.getElementById('navega').classList.toggle('mostrar');
+})
+
+
+lightbox.option({
+    'wrapAround': true,
+    'fadeDuration': 2000,
+    'maxWidth': 50,
+    'maxHeight': 50
+})
